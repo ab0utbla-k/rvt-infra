7ca56305e1fc0ee3077264836497f2268fe350ba
Ihar Statkevich <ihast@orsted.com>
2025-08-04 21:58:18 +0200

    Change health check path

--- implementation/alb.tf
+++ implementation/alb.tf
@@ -21,7 +21,7 @@ resource "aws_lb_target_group" "this" {
     unhealthy_threshold = 2
     timeout             = 5
     interval            = 30
-    path                = "/health"
+    path                = "/healthcheck"
     matcher             = "200"
     port                = "traffic-port"
     protocol            = "HTTP"
