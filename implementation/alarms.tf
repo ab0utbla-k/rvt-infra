7802b8428dfc2283f3c8f9225cabd2f67628d6f1
Ihar Statkevich <71011941+ab0utbla-k@users.noreply.github.com>
2025-08-06 12:45:42 +0200

    Add RDS Storage alarm

--- implementation/alarms.tf
+++ implementation/alarms.tf
@@ -5,10 +5,10 @@ resource "aws_sns_topic" "this" {
 resource "aws_cloudwatch_metric_alarm" "ecs_running_task_count" {
   alarm_name          = "${var.project_name}-ecs-running-tasks-low"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = 2
   metric_name         = "RunningTaskCount"
   namespace           = "ECS/ContainerInsights"
-  period              = "60"
+  period              = 60
   statistic           = "Average"
   threshold           = var.ecs_desired_count
   alarm_description   = "This metric monitors ECS running task count"
@@ -17,5 +17,23 @@ resource "aws_cloudwatch_metric_alarm" "ecs_running_task_count" {
   dimensions = {
     ServiceName = aws_ecs_service.this.name
     ClusterName = aws_ecs_cluster.this.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_free_storage_low" {
+  alarm_name          = "${var.project_name}-rds-free-storage-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = 300
+  statistic           = "Minimum"
+  threshold           = 10000000000
+  alarm_description   = "Alarm when RDS free storage drops below 10 GB"
+  alarm_actions       = [aws_sns_topic.this.arn]
+  treat_missing_data  = "breaching"
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.this.id
   }
 }
