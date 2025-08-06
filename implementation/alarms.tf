ba0497e8daefdd1f1c9df0bcab52cfc491aab411
Ihar Statkevich <71011941+ab0utbla-k@users.noreply.github.com>
2025-08-06 10:12:04 +0200

    Add first alarm

---
+++ implementation/alarms.tf
@@ -0,0 +1,29 @@
+resource "aws_sns_topic" "this" {
+  name = "${var.project_name}-alerts"
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  count     = var.alert_email != "" ? 1 : 0
+
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
+}
+
+resource "aws_cloudwatch_metric_alarm" "ecs_running_task_count" {
+  alarm_name          = "${var.project_name}-ecs-running-tasks-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "RunningTaskCount"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = var.ecs_desired_count
+  alarm_description   = "This metric monitors ECS running task count"
+  alarm_actions       = [aws_sns_topic.this.arn]
+
+  dimensions = {
+    ServiceName = aws_ecs_service.this.name
+    ClusterName = aws_ecs_cluster.this.name
+  }
+}
