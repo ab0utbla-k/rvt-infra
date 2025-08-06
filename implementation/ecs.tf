cbd46c01275e4db769f2e5526bb18cfc8396dc62
Ihar Statkevich <71011941+ab0utbla-k@users.noreply.github.com>
2025-08-06 15:44:22 +0200

    Forgot to add --spider

--- implementation/ecs.tf
+++ implementation/ecs.tf
@@ -55,7 +55,7 @@ resource "aws_ecs_task_definition" "this" {
       ]
 
       healthCheck = {
-        command     = ["CMD-SHELL", "wget --no-verbose --tries=1 http://localhost:${var.app_port}/healthcheck || exit 1"]
+        command     = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:${var.app_port}/healthcheck || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
