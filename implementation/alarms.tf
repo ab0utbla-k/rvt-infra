5bceb8d741a06531e8d4c136c8ea87b9ad8efaf5
Ihar Statkevich <71011941+ab0utbla-k@users.noreply.github.com>
2025-08-06 12:56:20 +0200

    Add RDS CPU alarm

--- implementation/alarms.tf
+++ implementation/alarms.tf
@@ -11,8 +11,10 @@ resource "aws_cloudwatch_metric_alarm" "ecs_running_task_count" {
   period              = 60
   statistic           = "Average"
   threshold           = var.ecs_desired_count
-  alarm_description   = "This metric monitors ECS running task count"
+  alarm_description   = "Triggers when the number of running ECS tasks falls below the desired count for 2 minutes"
   alarm_actions       = [aws_sns_topic.this.arn]
+
+  treat_missing_data  = "breaching"
 
   dimensions = {
     ServiceName = aws_ecs_service.this.name
@@ -29,9 +31,29 @@ resource "aws_cloudwatch_metric_alarm" "rds_free_storage_low" {
   period              = 300
   statistic           = "Minimum"
   threshold           = 10000000000
-  alarm_description   = "Alarm when RDS free storage drops below 10 GB"
+  alarm_description   = "Triggers when RDS free storage drops below 10 GB for 10 minutes"
   alarm_actions       = [aws_sns_topic.this.arn]
+
   treat_missing_data  = "breaching"
+
+  dimensions = {
+    DBInstanceIdentifier = aws_db_instance.this.id
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "rds_cpu_high" {
+  alarm_name          = "${var.project_name}-rds-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "Triggers when RDS CPU usage is over 80% for 10 minutes"
+  alarm_actions       = [aws_sns_topic.this.arn]
+
+  treat_missing_data  = "notBreaching"
 
   dimensions = {
     DBInstanceIdentifier = aws_db_instance.this.id
